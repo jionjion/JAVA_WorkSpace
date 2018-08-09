6234f61006ecea453d0c04d842869750be801278
jionjion <1434501783@qq.com>
2018-08-09 21:41:46 +0800

    提交修改

--- guider/src/main/java/com/jionjion/guider/GuiderApplication.java
+++ guider/src/main/java/com/jionjion/guider/GuiderApplication.java
@@ -3,11 +3,21 @@ package com.jionjion.guider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class GuiderApplication {
+public class GuiderApplication extends SpringBootServletInitializer{
 
+	
+	/** 启动入口 */
 	public static void main(String[] args) {
 		SpringApplication.run(GuiderApplication.class, args);
 	}
+	
+	/** Jar包入口,继承重写 */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
