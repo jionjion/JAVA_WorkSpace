8ef70667e4958230135facbc81971bb0bf0f39d6
jionjion <1434501783@qq.com>
2019-04-26 19:30:12 +0800

    no message

--- api/src/main/java/top/jionjion/api/exception/auth/UserException.java
+++ api/src/main/java/top/jionjion/api/exception/auth/UserException.java
@@ -1,13 +1,14 @@
 package top.jionjion.api.exception.auth;
 
-import org.springframework.context.annotation.Bean;
+import lombok.Getter;
 import top.jionjion.api.exception.ApiException;
 
 /**
  * @author Jion
  *  用户认证中的异常信息
  */
+@Getter
 public class UserException extends ApiException {
 
-
+    
 }
