9deb7276f78822788d8f013383d52009ffc4662e
jionjion <1434501783@qq.com>
2019-05-17 18:04:10 +0800

    Jackson练习

---
+++ api/src/test/framework/jackson/annotation/JsonPropertyOrderTest.java
@@ -0,0 +1,56 @@
+package jackson.annotation;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Jion
+ * \@JsonPropertyOrder 注解使用
+ */
+@Slf4j
+@JsonPropertyOrder({"id","name"})
+public class JsonPropertyOrderTest {
+
+    /**
+     * 内部类
+     */
+    public class Student {
+
+        private Integer id;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @Test
+    public void test() throws JsonProcessingException {
+        Student student = new Student();
+        student.setId(1);
+        student.setName("囧囧");
+
+        String result = new ObjectMapper().writeValueAsString(student);
+        assertNotNull(result);
+        log.info(result);
+    }
+}
---
+++ api/src/test/framework/jackson/annotation/JsonRootNameTest.java
@@ -0,0 +1,60 @@
+package jackson.annotation;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Jion
+ * \@JsonRootName 注解使用
+ */
+@Slf4j
+public class JsonRootNameTest {
+
+    /**
+     * 内部类
+     */
+    @JsonRootName(value = "student")
+    public class Student {
+
+        private Integer id;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
+    @Test
+    public void test() throws JsonProcessingException {
+        Student student = new Student();
+        student.setId(1);
+        student.setName("囧囧");
+
+        String result = new ObjectMapper()
+                .enable(SerializationFeature.WRAP_ROOT_VALUE)
+                .writeValueAsString(student);
+        assertNotNull(result);
+        log.info(result);
+    }
+}
