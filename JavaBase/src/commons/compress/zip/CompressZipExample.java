2ba32eb8be53241e300128d7ad6661095445e0a4
jionjion <1434501783@qq.com>
2019-05-25 18:10:40 +0800

    新增commons-compress

---
+++ JavaBase/src/commons/compress/zip/CompressZipExample.java
@@ -0,0 +1,23 @@
+package commons.compress.zip;
+
+import org.junit.Test;
+
+/**
+ * @author Jion
+ *  压缩文件为Zip
+ */
+public class CompressZip {
+
+    /** 压缩单个文件 */
+    @Test
+    public void compressFileToZip(){
+        
+    }
+
+
+    /** 压缩文件夹及其子文件 */
+
+
+
+
+}
