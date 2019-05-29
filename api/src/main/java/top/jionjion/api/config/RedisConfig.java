0558464d683057988167baf2a10780a3ca540e15
jionjion <1434501783@qq.com>
2019-05-29 22:44:50 +0800

    redis配置

--- api/src/main/java/top/jionjion/api/config/RedisConfig.java
+++ api/src/main/java/top/jionjion/api/config/RedisConfig.java
@@ -3,6 +3,8 @@ package top.jionjion.api.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
@@ -10,12 +12,17 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import javax.crypto.KeyGenerator;
+import java.time.Duration;
 
 /**
  * @author Jion
@@ -27,7 +34,6 @@ import javax.crypto.KeyGenerator;
 @EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
 
-
     @Value("${spring.redis.host}")
     private String host;
 
@@ -37,5 +43,38 @@ public class RedisConfig extends CachingConfigurerSupport {
     @Value("${spring.redis.timeout}")
     private int timeout;
 
+    @Value("${spring.redis.password}")
+    private String password;
 
+    @Value("${spring.redis.database}")
+    private int database;
+
+
+
+    /** 创建连接工厂 */
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        // 连接配置类
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
+        redisStandaloneConfiguration.setDatabase(database);
+
+        // Jedis连接配置
+        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.defaultConfiguration();
+        JedisClientConfiguration.builder().connectTimeout(Duration.ofMillis(timeout));
+
+        // 根据配置创建连接工厂
+        return new JedisConnectionFactory(redisStandaloneConfiguration,jedisClientConfiguration);
+
+    }
+
+    /** 重新配置 RedisTemplate */
+    public RedisTemplate redisTemplate(){
+        JedisConnectionFactory jedisConnectionFactory = redisConnectionFactory();
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        return redisTemplate;
+    }
 }
--- api/src/test/java/top/jionjion/api/config/RedisConfigTest.java
+++ api/src/test/java/top/jionjion/api/config/RedisConfigTest.java
@@ -3,7 +3,10 @@ package top.jionjion.api.config;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import springData.jpa.repositoryTest.SpringDataJpaBaseTest;
 import top.jionjion.api.BaseTest;
 
@@ -16,14 +19,50 @@ import static org.junit.Assert.*;
 @Slf4j
 public class RedisConfigTest extends BaseTest{
 
+/*
+Interface	                Description
+                            Key Type Operations
+GeoOperations               Redis geospatial operations, such as GEOADD, GEORADIUS,…​
+HashOperations              Redis hash operations
+HyperLogLogOperations       Redis HyperLogLog operations, such as PFADD, PFCOUNT,…​
+ListOperations              Redis list operations
+SetOperations               Redis set operations
+ValueOperations             Redis string (or value) operations
+ZSetOperations              Redis zset (or sorted set) operations
+Key Bound Operations        BoundGeoOperations
+                            Redis key bound geospatial operations
+BoundHashOperations         Redis hash key bound operations
+BoundKeyOperations          Redis key bound operations
+BoundListOperations         Redis list key bound operations
+BoundSetOperations          Redis set key bound operations
+BoundValueOperations        Redis string (or value) key bound operations
+BoundZSetOperations         Redis zset (or sorted set) key bound operations
+
+参考 org.springframework.data.redis.support
+ */
+
+
+    @Autowired
+    RedisConfig redisConfig;
+
     @Autowired
     RedisTemplate<String, String> redisTemplate;
 
-    @Test
-    public void test(){
+    @Autowired
+    JedisConnectionFactory jedisConnectionFactory;
 
-        String result = redisTemplate.opsForValue().get("user");
-        assertNotNull(result);
-        log.info(result);
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    /** 测试注入连接工厂 */
+    @Test
+    public void testAutowiredBean(){
+        assertNotNull(redisConfig);
+        assertNotNull(redisTemplate);
+        assertNotNull(jedisConnectionFactory);
+        assertNotNull(stringRedisTemplate);
+
     }
+
+
 }
