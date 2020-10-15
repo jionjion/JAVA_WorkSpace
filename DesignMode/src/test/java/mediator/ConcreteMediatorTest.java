ecca286f943a908d3f11cea610c48fc3cf1f8260
JionJion <1434501783@qq.com>
2020-10-15 22:07:12 +0800

    2020-10-15

---
+++ DesignMode/src/test/java/mediator/ConcreteMediatorTest.java
@@ -0,0 +1,25 @@
+package mediator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *  中介者模式测试
+ * @author Jion
+ */
+public class ConcreteMediatorTest {
+
+    @Test
+    public void test(){
+        // 创建一个中介者
+        Mediator mediator = new ConcreteMediator();
+        // 创建各个子系统
+        Alarm alarm = new Alarm(mediator, "alarm");
+        Tv tv = new Tv(mediator, "tv");
+        Light light = new Light(mediator, "light");
+        // 发送消息
+        alarm.sendMessage(1);
+        light.sendMessage(2);
+    }
+}
